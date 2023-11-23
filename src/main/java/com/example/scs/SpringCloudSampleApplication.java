0aba72583d8cd7fa43e26b9583eb5e296a0bfb8f
DongJu-Na <ehdeld123@naver.com>
2023-11-23 18:16:06 +0900

    project initializing
    프로젝트 초기 설정 진행중

---
+++ src/main/java/com/example/scs/SpringCloudSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.scs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scs/SpringCloudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
